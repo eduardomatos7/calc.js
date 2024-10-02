1a93c4dbc88174128a9a1a8b1d588b498e35e6f1
eduardomatos7 <eduardo.matoss@ufpe.br>
2024-10-02 14:42:12 -0300

    adding click function to buttons

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const input = document.getElementById('input')
 const resultInput = document.getElementById('result')
 const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
 
+document.querySelectorAll('.charKey').forEach(function(charKeyBtn){
+    charKeyBtn.addEventListener('click', function(){
+        const value = charKeyBtn.dataset.value
+        input.value += value
+
+    })
+})
+
 input.addEventListener('keydown', function(ev){
     ev.preventDefault()
     if (allowedKeys.includes(ev.key)){
@@ -13,4 +21,11 @@ input.addEventListener('keydown', function(ev){
     if (ev.key === 'Backspace'){
         input.value = input.value.slice(0, -1) // retorna a função ate o penultimo caracter
     }
+    if (ev.key === 'Enter'){
+        calculate()
+    }
 })
+
+function calculate(){
+
+}
